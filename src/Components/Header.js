1ee3036340d8b06a21e4241b75ad02754c2951aa
Gagan Deep Kaur <igdkaur@gmail.com>
2021-08-23 07:38:09 +0530

    add header component

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeaderStyles from '../Styles/Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      Amazon Cart
+    </div>
+  )
+}
+
+export default Header
