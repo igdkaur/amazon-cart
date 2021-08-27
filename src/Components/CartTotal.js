5fcf23da4a870aa0bb28e21e5dc6f7a31f97be52
Gagan Deep Kaur <igdkaur@gmail.com>
2021-08-27 07:56:13 +0530

    cart total styling

--- src/Components/CartTotal.js
+++ src/Components/CartTotal.js
@@ -5,6 +5,15 @@ import '../Styles/CartTotal.css'
 const CartTotal = () => {
   return (
     <div className="CartTotal">
+      <h3>
+        Subtotal(5 items):
+        <span className="CartTotal-price">
+          $2,172.38
+        </span>
+      </h3>
+      <button>
+        Proceed to checkout
+      </button>
       
     </div>
   )
